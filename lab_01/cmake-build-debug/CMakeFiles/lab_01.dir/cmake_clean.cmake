3d02f6fc0a6c1a03aaf563de8565f2f1e55ea190
m4kson <limos.00@mail.ru>
2022-01-29 21:44:44 +0300

    main.c added

---
+++ lab_01/cmake-build-debug/CMakeFiles/lab_01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_01.dir/main.c.o"
+  "lab_01"
+  "lab_01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab_01/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define OK 0
+
+static int enter_coordinates(int *x_1, int *x_2, int *x_3, int *x_4, int *x_5, int *x_6);
+static int perimetr_count(int x_1, int x_2, int x_3, int x_4, int x_5, int x_6);
+static void result_print(int P);
+
+int main()
+{
+    int rc = 0;
+
+    return rc;
+}
+
+static int enter_coordinates(int *x_1, int *x_2, int *x_3, int *x_4, int *x_5, int *x_6)
+{
+    int rc = OK;
+    
+
+}
+
