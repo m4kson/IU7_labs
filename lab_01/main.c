7a0c9408d2f390cd441f89c8f44e95fd60f7dc34
m4kson <limos.00@mail.ru>
2022-02-02 16:37:09 +0300

    update main.c

--- lab_01/main.c
+++ lab_01/main.c
@@ -6,121 +6,81 @@
 #define MAX_STRING_LEN 500
 
 #define READ_ERROR -1
-#define NON_A_TRIANGLE -2
+#define NOT_A_TRIANGLE -2
 
-static int enter_coordinates(int *x_1, int *y_1, int *x_2, int *y_2, int *x_3, int *y_3);
-static int perimetr_count(int x_1, int y_1, int x_2, int y_2, int x_3, int y_3);
-static void result_print(int x_1, int y_1, int x_2, int y_2, int x_3, int y_3, int P);
-static int read_number(int *x, char *string);
+static int enter_coordinates(int *vertices);
+static double perimetr_count(int *vertices);
+static void result_print(int *vertices, double P);
 
 int main()
 {
     int rc = 0;
-    int x_1, y_1, x_2, y_2, x_3, y_3;
+    int vertices[6];
+    double P;
 
-    rc = enter_coordinates(&x_1, &y_1, &x_2, &y_2, &x_3, &y_3);
+    rc = enter_coordinates(vertices);
     if (rc == OK)
     {
-        rc = perimetr_count(x_1, y_1, x_2, y_2, x_3, y_3);
-        if (rc >= 0)
+        P = perimetr_count(vertices);
+        if (P > 0)
         {
-            result_print(x_1, y_1, x_2, y_2, x_3, y_3, rc);
-            rc = OK;
+            result_print(vertices, P);
         }
     }
 
     return rc;
 }
 
-static int enter_coordinates(int *x_1, int *y_1, int *x_2, int *y_2, int *x_3, int *y_3)
+static int enter_coordinates(int *vertices)
 {
     int rc = OK;
-    char *string = "";
-    int errno;
-    int EINTR;
+    char string[MAX_STRING_LEN];
+    char *end = string;
 
     printf("enter coordinates:");
 
-    fgets(string, MAX_STRING_LEN, stdin);
-
-    rc = read_number(x_1, string);
-    if (rc != OK)
-    {
-        return rc;
-    }
-    rc = read_number(y_1,string);
-    if (rc != OK)
-    {
-        return rc;
-    }
-    rc = read_number(x_2,string);
-    if (rc != OK)
-    {
-        return rc;
-    }
-    rc = read_number(y_2, string);
-    if (rc != OK)
-    {
-        return rc;
-    }
-    rc = read_number(x_3, string);
-    if (rc != OK)
-    {
-        return rc;
-    }
-    rc = read_number(y_3,string);
-    if (rc != OK)
-    {
-        return rc;
-    }
-
-    return rc;
-}
-
-static int read_number(int *x, char *string)
-{
-    int rc = OK;
-//    char *end;
-//    *x = strtol(string, &end, 0);
-//
-//    if (*end != ' ' && *end != '\n')
-//    {
-//        rc = READ_ERROR;
-//        *x = 0;
-//    }
-//
-//    string = end;
-
-    if (scanf("%d", x) != 1)
+    if (fgets(string, MAX_STRING_LEN, stdin) == NULL)
     {
         rc = READ_ERROR;
     }
-
+    else
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            vertices[i] = strtol(end, &end, 10);
+            if (*end != ' ' && *end != '\n')
+            {
+                rc = READ_ERROR;
+                break;
+            }
+        }
+    }
     return rc;
 }
 
-static int perimetr_count(int x_1, int y_1, int x_2, int y_2, int x_3, int y_3)
+static double perimetr_count(int *vertices)
 {
-    int rc = OK;
-    if (x_1 == x_2 == x_3 || y_1 == y_2 == y_3)
+    double rc = OK;
+    if (0) //check if triangle
     {
-        rc = NON_A_TRIANGLE;
+
     }
     else
     {
-        rc = sqrt(pow(x_1 - x_2, 2) + pow(y_1 - y_2, 2)) + sqrt(pow(x_1 - x_3, 2) + pow(y_1 - y_3, 2)) + \
-        + sqrt(pow(x_2 - x_3, 2) + pow(y_2 - y_3, 2));
+        rc = sqrt(pow(vertices[0] - vertices[2], 2) + pow(vertices[1] - vertices[3], 2)) + sqrt(pow(vertices[0]
+        - vertices[4], 2) + pow(vertices[1] - vertices[5], 2)) + sqrt(pow(vertices[2] - vertices[4], 2) +
+        pow(vertices[3] - vertices[5], 2));
     }
 
     return rc;
 }
 
-static void result_print(int x_1, int y_1, int x_2, int y_2, int x_3, int y_3, int P)
+static void result_print(int *vertices, double P)
 {
     printf("your coordinates is:\n x_1 - %d\n y_1 - %d\n x_2 - %d\n y_2 - %d\n x_3 - %d\n y_3 - %d\n",\
-    x_1, y_1, x_2, y_2, x_3, y_3);
-    printf("AB - %d\n", (int)sqrt(pow(x_1 - x_2, 2) + pow(y_1 - y_2, 2)));
-    printf("AC - %d\n", (int)sqrt(pow(x_1 - x_3, 2) + pow(y_1 - y_3, 2)));
-    printf("BC - %d\n", (int)sqrt(pow(x_2 - x_3, 2) + pow(y_2 - y_3, 2)));
-    printf("Result is: %d\n", P);
+    vertices[0], vertices[1], vertices[2], vertices[3], vertices[4], vertices[5]);
+    printf("AB - %lf\n", sqrt(pow(vertices[0] - vertices[2], 2) + pow(vertices[1] - vertices[3], 2)));
+    printf("AC - %lf\n", sqrt(pow(vertices[0]- vertices[4], 2) + pow(vertices[1] - vertices[5], 2)));
+    printf("BC - %lf\n", sqrt(pow(vertices[2] - vertices[4], 2) + pow(vertices[3] - vertices[5], 2)));
+    printf("Result is: %lf\n", P);
 }
